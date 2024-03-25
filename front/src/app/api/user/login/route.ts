0fa8d59aa57a2abb6b9d943f2042703251a4083d
Rlack97 <amalilith97@gamil.com>
2024-03-25 17:45:48 +0900

    api get 4

--- front/src/app/api/user/login/route.ts
+++ front/src/app/api/user/login/route.ts
@@ -1,27 +1,28 @@
 import { sql } from "@vercel/postgres";
 
+import { NextResponse } from "next/server";
+
 export async function POST(req: Request) {
   const body = await req.json();
   const { userId, password } = body;
 
-  // 사용자 검증
-
   try {
+    // 사용자 검증
     const { rows } =
       await sql`SELECT * FROM users WHERE user_id = ${userId} AND user_password = ${password} LIMIT 1`;
 
     if (rows.length > 0) {
       // 로그인 성공
-      return {
-        status: 200,
-        body: { message: "로그인 성공", user_id: userId },
-      };
+      return NextResponse.json(
+        { message: "로그인 성공", user_id: userId },
+        { status: 200 }
+      );
     } else {
       // 로그인 실패 - 에러 처리
-      return {
-        status: 401,
-        body: { error: "아이디 또는 비밀번호가 일치하지 않습니다." },
-      };
+      return NextResponse.json(
+        { error: "아이디 또는 비밀번호가 일치하지 않습니다." },
+        { status: 401 }
+      );
     }
   } catch (error) {
     console.error("로그인 중 오류 발생:", error);
