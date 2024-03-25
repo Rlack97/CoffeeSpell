235875a34dad09ab7ebbaae6c0c29793388bf3f0
Rlack97 <amalilith97@gamil.com>
2024-03-25 20:26:07 +0900

    menu api 6

--- front/src/app/setting/menu/page.tsx
+++ front/src/app/setting/menu/page.tsx
@@ -32,6 +32,7 @@ export default function Menu() {
     try {
       const apiUrl = "/api/menu";
       const response = await axios.get(apiUrl);
+      console.log(response.data.message);
       setMenuItems(response.data.message);
     } catch (error) {
       console.error("에러 발생:", error);
