053b64d94059fa3871b2cfc757ec3fc0b93d6b60
Rlack97 <amalilith97@gamil.com>
2024-03-25 14:22:11 +0900

    menu get rows take4

--- front/src/app/api/menu/route.ts
+++ front/src/app/api/menu/route.ts
@@ -3,12 +3,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
-    const result = await sql`SELECT * FROM menu`;
+    const { rows } = await sql`SELECT * FROM menu`;
 
     // 메뉴가 존재하지 않는 경우에도 그냥 빈 값을 전달
-    const menu = result.rows ?? [];
+    // const menu = rows ?? [];
 
-    return NextResponse.json({ menu }, { status: 200 });
+    return NextResponse.json({ rows }, { status: 200 });
   } catch (error) {
     console.error("에러 발생:", error);
     return NextResponse.json(
