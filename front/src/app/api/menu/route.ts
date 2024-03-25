001878970c62b3be8780c2d3a3294c58512249bb
Rlack97 <amalilith97@gamil.com>
2024-03-25 17:33:34 +0900

    api get

--- front/src/app/api/menu/route.ts
+++ front/src/app/api/menu/route.ts
@@ -1,19 +1,21 @@
 import { sql } from "@vercel/postgres";
-import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function GET(req: Request) {
   try {
     const { rows } = await sql`SELECT * FROM menu`;
 
     // 메뉴가 존재하지 않는 경우에도 그냥 빈 값을 전달
     // const menu = rows ?? [];
 
-    return NextResponse.json({ rows }, { status: 200 });
+    return {
+      status: 200,
+      body: { message: rows },
+    };
   } catch (error) {
     console.error("에러 발생:", error);
-    return NextResponse.json(
-      { error: "서버에서 오류가 발생했습니다." },
-      { status: 500 }
-    );
+    return {
+      status: 500,
+      body: { error: "서버에서 오류가 발생했습니다." },
+    };
   }
 }
