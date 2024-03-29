887f558ce206ed0df81abb91e48e242b23487d90
Rlack97 <amalilith97@gamil.com>
2024-03-29 12:59:02 +0900

    counter api debug

--- front/src/app/api/pusher/route.ts
+++ front/src/app/api/pusher/route.ts
@@ -19,19 +19,17 @@ export async function POST(req: Request) {
     const body = await req.json();
     const message = body;
 
-    pusher
-      .trigger("my-channel", "my-event", { message })
-      .then(() => {
-        NextResponse.json({ message: "전송 성공" }, { status: 200 });
-      })
-      .catch((error) => {
-        console.error("Error sending message:", error);
-        NextResponse.json(
-          { error: "서버에서 오류가 발생했습니다." },
-          { status: 500 }
-        );
-      });
+    try {
+      await pusher.trigger("my-channel", "my-event", { message });
+      return NextResponse.json({ message: "전송 성공" }, { status: 200 });
+    } catch (error) {
+      console.error("Error sending message:", error);
+      return NextResponse.json(
+        { error: "서버에서 오류가 발생했습니다." },
+        { status: 500 }
+      );
+    }
   } else {
-    NextResponse.json({ success: false, message: "Method Not Allowed" });
+    return NextResponse.json({ success: false, message: "Method Not Allowed" });
   }
 }
