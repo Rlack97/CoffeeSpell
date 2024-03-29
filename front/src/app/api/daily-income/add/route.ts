0da2544ffe798e422c860d9c13b9a57865d92906
Rlack97 <amalilith97@gamil.com>
2024-03-29 20:41:45 +0900

    daily income save

--- front/src/app/api/daily-income/add/route.ts
+++ front/src/app/api/daily-income/add/route.ts
@@ -8,15 +8,12 @@ export async function POST(req: NextRequest) {
     const incomeNumber = parseInt(income);
     const { rows } =
       await sql`SELECT user_pk FROM users WHERE user_id = ${userId}`;
+    console.error(rows);
+    console.error(userId);
 
-    if (rows && rows.length > 0) {
-      const userPk = rows[0].user_pk;
-      await sql`INSERT INTO daily_income (date, daily_income, user_id) VALUES (${today}, ${incomeNumber}, ${userPk})`;
-      return NextResponse.json({ message: "기록되었습니다." }, { status: 200 });
-    } else {
-      // 사용자를 찾을 수 없는 경우에 대한 처리
-      console.error("사용자를 찾을 수 없습니다.");
-    }
+    const userPk = rows[0].user_pk;
+    await sql`INSERT INTO daily_income (date, daily_income, user_id) VALUES (${today}, ${incomeNumber}, ${userPk})`;
+    return NextResponse.json({ message: "기록되었습니다." }, { status: 200 });
   } catch (error) {
     console.error("에러 발생:", error);
     return NextResponse.json(
