584941beb2d518886f01cafcfe97c6cd21f819e1
Rlack97 <amalilith97@gamil.com>
2024-08-17 00:22:33 +0900

    menu add & delete

--- front/src/app/setting/menu/page.tsx
+++ front/src/app/setting/menu/page.tsx
@@ -32,6 +32,7 @@ export default function Menu() {
     try {
       const apiUrl = "/api/menu";
       const response = await axios.get(apiUrl);
+      console.log(response.data.rows);
       setMenuItems(response.data.rows);
     } catch (error) {
       console.error("에러 발생:", error);
