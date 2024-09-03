cfc7724599a10d497631d2aefd9b042b4c22f837
Rlack97 <amalilith97@gamil.com>
2024-09-03 23:05:00 +0900

    please no cahche

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -4,16 +4,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/counter',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 's-maxage=60, stale-while-revalidate=59', // 캐시를 60초 동안 유지하고 그 이후에 재검증
-          },
-        ],
-      },
-      {
-        source: '/setting',
+        source: '/api',
         headers: [
           {
             key: 'Cache-Control',
