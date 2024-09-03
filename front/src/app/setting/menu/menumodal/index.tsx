8c6308f87b35131dbead8647f98c0b457c689b2c
Rlack97 <amalilith97@gamil.com>
2024-09-03 23:54:33 +0900

    menu update modal

--- front/src/app/setting/menu/menumodal/index.tsx
+++ front/src/app/setting/menu/menumodal/index.tsx
@@ -219,7 +219,7 @@ export default function MenuModal({
                   name="name"
                   value={formData.name}
                   onChange={handleInputChange}
-                  className="bg-gray-200 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-base border-gray-300 rounded-md py-3"
+                  className="bg-gray-200 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-base border-gray-300 rounded-md py-2 px-4 "
                 />
               </div>
               {/* 메뉴 가격 수정 폼 */}
@@ -236,7 +236,7 @@ export default function MenuModal({
                   name="price"
                   value={formData.price}
                   onChange={handleInputChange}
-                  className="bg-gray-200 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-base border-gray-300 rounded-md py-3"
+                  className="bg-gray-200 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-base border-gray-300 rounded-md py-2 px-4"
                 />
               </div>
               {/* 메뉴 카테고리 수정 폼 */}
@@ -253,7 +253,7 @@ export default function MenuModal({
                   name="category"
                   value={formData.category}
                   onChange={handleInputChange}
-                  className="bg-gray-200 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-base border-gray-300 rounded-md py-3"
+                  className="bg-gray-200 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-base border-gray-300 rounded-md py-2 px-4"
                 />
               </div>
               <div className="flex justify-end">
